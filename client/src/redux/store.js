559bcb7ba87390c6a1bf1fa5ef008d1186a4e22f
sudeen <ranjitkarsudeen14@gmail.com>
2024-04-23 21:15:32 +1000

    Added dark mode functionality

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import themeReducer from "./theme/themeSlice";
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer,
 });
 
 const peristConfig = {
