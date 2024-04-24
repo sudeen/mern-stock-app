e50cdc673c3067606cbb039d14030a323bd3643d
sudeen <ranjitkarsudeen14@gmail.com>
2024-04-25 01:38:31 +1000

    delete post functionality added

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createPost, getposts } from "../controllers/post.controller.js";
+import {
+  createPost,
+  deletepost,
+  getposts,
+  updatepost,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createPost);
 router.get("/getposts", getposts);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 
 export default router;
