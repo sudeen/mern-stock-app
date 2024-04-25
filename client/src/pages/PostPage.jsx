99a6f8bdf0e4ac29daf841ee7142e7ddeb26b9f8
sudeen <ranjitkarsudeen14@gmail.com>
2024-04-25 21:27:05 +1000

    Added call to action to post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,7 +1,7 @@
 import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-// import CallToAction from "../components/CallToAction";
+import CallToAction from "../components/CallToAction";
 // import CommentSection from "../components/CommentSection";
 // import PostCard from "../components/PostCard";
 
@@ -86,10 +86,10 @@ export default function PostPage() {
         className="p-3 max-w-2xl mx-auto w-full post-content" // post-content is custom css for styling
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
-      {/* <div className="max-w-4xl mx-auto w-full">
+      <div className="max-w-4xl mx-auto w-full">
         <CallToAction />
       </div>
-      <CommentSection postId={post._id} /> */}
+      {/* <CommentSection postId={post._id} /> */}
 
       <div className="flex flex-col justify-center items-center mb-5">
         <h1 className="text-xl mt-5">Recent articles</h1>
