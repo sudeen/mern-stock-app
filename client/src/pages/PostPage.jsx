31fa66ff216723c7e9133b19d535d37c781bb8c2
sudeen <ranjitkarsudeen14@gmail.com>
2024-04-25 22:20:45 +1000

    Show comment section and comments by users

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,7 +2,7 @@ import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
-// import CommentSection from "../components/CommentSection";
+import CommentSection from "../components/CommentSection";
 // import PostCard from "../components/PostCard";
 
 export default function PostPage() {
@@ -89,7 +89,7 @@ export default function PostPage() {
       <div className="max-w-4xl mx-auto w-full">
         <CallToAction />
       </div>
-      {/* <CommentSection postId={post._id} /> */}
+      <CommentSection postId={post._id} />
 
       <div className="flex flex-col justify-center items-center mb-5">
         <h1 className="text-xl mt-5">Recent articles</h1>
