9bd99df4514c1c16039f68c1953d51f69a78e7a6
sudeen <ranjitkarsudeen14@gmail.com>
2024-04-25 19:52:15 +1000

    Show users in admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPosts";
-// import DashUsers from "../components/DashUsers";
+import DashUsers from "../components/DashUsers";
 // import DashComments from "../components/DashComments";
 // import DashboardComp from "../components/DashboardComp";
 
@@ -27,6 +27,8 @@ export default function Dashboard() {
       {tab === "profile" && <DashProfile />}
       {/* posts... */}
       {tab === "posts" && <DashPosts />}
+      {/* users */}
+      {tab === "users" && <DashUsers />}
     </div>
   );
 }
