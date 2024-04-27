626fe4166ecb9f8a2e5f67e3062a57d120f919fc
sudeen <ranjitkarsudeen14@gmail.com>
2024-04-27 17:45:29 +1000

    Setting up CORS

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
@@ -24,6 +25,7 @@ const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!");
@@ -34,13 +36,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-// If you are using CRA you put /client/build but on VITE you have to using /client/dist
+// If you are using CRA you put /client/build but on VITE you have to use /client/dist
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
+// This section is global and can be used in any page or component for error handling
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
